6920d27a0404701bfdfbb8c79cc6e9ef233cc6ae
linda <lindaochem@gmail.com>
2024-03-20 11:40:25 +0100

    new build

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,12 +1,26 @@
 const {createProxyMiddleware} = require('http-proxy-middleware');
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      // 👇️ make sure to update your target
-      target: 'http://localhost:4000',
+      // Update the target to point to the backend container's hostname within Docker network
+      target: 'http://cleowoman/backend:4000', // Replace 'backend-container-name' with the actual name of your backend container
       changeOrigin: true,
     }),
   );
 };
+
+
+// module.exports = function (app) {
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//       // 👇️ make sure to update your target
+//       target: 'http://localhost:4000',
+//       changeOrigin: true,
+//     }),
+//   );
+// };
